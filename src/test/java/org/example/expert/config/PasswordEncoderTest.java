1fd8c9b0503fdb16d3df39f7308eb902ea04e758
BullGombo <ormaju15123647@gmail.com>
2025-12-05 12:52:52 +0900

    레벨 4: 테스트코드 연습
    - PasswordEncoderTest 클래스에 있는 matches_메서드가_정상적으로_동작한다() 테스트가 의도대로 성공할 수 있게 수정
    
    - ManagerServiceTest 의 클래스에 있는 manager_목록_조회_시_Todo가_없다면_NPE_에러를_던진다() 테스트가 성공하고 컨텍스트와 일치하도록 테스트 코드와 테스트 코드 메서드명을 수정
    
    - CommentServiceTest 의 클래스에 있는 comment_등록_중_할일을_찾지_못해_에러가_발생한다() 테스트가 성공할 수 있도록 테스트 코드를 수정
    
    - ManagerServiceTest 클래스에 있는 todo의_user가_null인_경우_예외가_발생한다() 테스트가 성공할 수 있도록 서비스 로직을 수정

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -22,7 +22,8 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        // matches() 메서드를 컨트롤 우클릭해서 보면, 첫번째 인자값에 raw가 들어가야한다. 따라서 순서만 바꿔줌
+        // matches() 메서드를 컨트롤 우클릭해서 보면, 첫번째 인자값에 raw가 들어가야한다.
+        // 따라서 순서만 바꿔줌
         boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
