c7651604c3029db7e5f360dbe5366625de7bd351
BullGombo <ormaju15123647@gmail.com>
2025-12-05 12:12:23 +0900

    레벨 3: N+1 문제 기존 @Query를 사용한 fetch join을 삭제, @EntityGraph(attributePaths = "user")로 user 즉시 로드

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,11 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    // Lv 3. N+1 문제
+//    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+//    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    // 페이징 사용 시 fetch join은 제약이 존재하기 때문에, 간단한 조인 조건 제어는 엔티티그래프로 처리
+    @EntityGraph(attributePaths = "user")  // 연관된 User 엔티티 미리 로드
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
